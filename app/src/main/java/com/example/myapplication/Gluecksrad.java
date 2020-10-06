3a5cea1ac0d2b820a4bf7b2806f500003bb990bc
404Pino <72466693+404Pino@users.noreply.github.com>
2020-10-06 21:40:14 +0200

    asdf

--- app/src/main/java/com/example/myapplication/Gluecksrad.java
+++ app/src/main/java/com/example/myapplication/Gluecksrad.java
@@ -6,23 +6,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-public class Gluecksrad extends AppCompatActivity {
+import java.util.Random;
 
-    public ImageView  myImage ;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+public class Gluecksrad extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gluecksrad);
-        final Animation myRotation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotation);
-        ((ImageButton)findViewById(R.id.imageButton)).setOnClickListener(new View.OnClickListener()    {
+        setContentView(R.layout.activity_main);
+
+        final ImageView myView = (ImageView)findViewById(R.id.glueck);
+        final Animation rotation = AnimationUtils.loadAnimation(this, R.anim.rotation);
+        rotation.setRepeatCount(Animation.INFINITE);
+        Button button = (Button)findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View arg0) {
-                myImage.startAnimation(myRotation);
+            public void onClick(View v) {
+                animation(myView,rotation);
             }
         });
+
+    }
+    protected void animation(ImageView view, Animation rotation) {
+        Random generator = new Random();
+        for (int i = 0; i < generator.nextInt(); i++) {
+            view.startAnimation(rotation);
+        }
     }
 
 }
